2ffdb348e816437418cb2715c6c85ed47134a0b1
Maolei Tang <tangmaolei@tangmaoleideMacBook-Pro.local>
2017-04-23 20:20:28 -0700

    intitial architecture

---
+++ src/main/java/com/mangokiwi/TeachTechApplication.java
@@ -0,0 +1,12 @@
+package com.mangokiwi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeachTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeachTechApplication.class, args);
+	}
+}
