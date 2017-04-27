deaf23fc52509b10dc6baa04e55f3fbdfa0900a3
Maolei Tang <tangmaolei@tangmaoleideMacBook-Pro.local>
2017-04-27 01:08:52 -0700

    delete useless dependencies in pom.xml

--- src/main/java/com/mangokiwi/TeachTechApplication.java
+++ src/main/java/com/mangokiwi/TeachTechApplication.java
@@ -2,11 +2,19 @@ package com.mangokiwi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
-public class TeachTechApplication {
+public class TeachTechApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TeachTechApplication.class);
+	}
+
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(TeachTechApplication.class, args);
 	}
 }
