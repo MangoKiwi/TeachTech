8915c9c465260a8da3eacba5fb4e9b7eef94ffd8
Feng <patrickfeng568@gmail.com>
2017-04-27 18:22:12 -0700

    security config update

--- src/main/java/com/mangokiwi/config/DataConfig.java
+++ src/main/java/com/mangokiwi/config/DataConfig.java
@@ -67,6 +67,8 @@ public class DataConfig {
         Properties properties = new Properties();
         properties.put("hibernate.dialect", environment.getProperty("hibernate.dialect"));
         properties.put("hibernate.implicit_naming_strategy", environment.getProperty("hibernate.implicit_naming_strategy"));
+        properties.put("hibernate.format_sql", environment.getProperty("hibernate.format_sql"));
+        properties.put("hibernate.show_sql", environment.getProperty("hibernate.show_sql"));
         properties.put("hibernate.hbm2ddl.auto", environment.getProperty("hibernate.hbm2ddl.auto"));
         return properties;
     }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,8 +1,8 @@
 # noinspection SqlNoDataSourceInspectionForFile
 
 -- Insert role
-insert into role (name) values ('ROLE_USER');
+insert into Role (name) values ('ROLE_USER');
 
 -- Insert two users (passwords are both 'password')
-insert into user (username,enabled,password,role_id) values ('user',true,'password',1);
-insert into user (username,enabled,password,role_id) values ('user2',true,'password',1);
+insert into User (username,enabled,password,role_id) values ('user',true,'password',1);
+insert into User (username,enabled,password,role_id) values ('user2',true,'password',1);
