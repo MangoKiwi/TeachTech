7292ad7bde1031823312323f5a0a37d8451e3da6
Feng <patrickfeng568@gmail.com>
2017-05-08 23:12:12 -0700

    update User model add appointment and Tag model

---
+++ src/main/java/com/mangokiwi/model/AppointmentStatus.java
@@ -0,0 +1,12 @@
+package com.mangokiwi.model;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by zhenfeng on 5/8/17.
+ */
+
+
+public enum AppointmentStatus {
+    FINISHED,SCHEDULED,EXPECT_RATING
+}
--- src/main/java/com/mangokiwi/model/User.java
+++ src/main/java/com/mangokiwi/model/User.java
@@ -1,22 +1,56 @@
 package com.mangokiwi.model;
 
+
 import javax.persistence.*;
-import javax.validation.constraints.Size;
-import java.util.Set;
+import java.util.Date;
 
 @Entity
 public class User {
+
     @Id
     private Long id;
 
     @Column(unique = true)
     private String username;
 
+    private String firstName;
+
+    private String lastName;
+
+    @Enumerated(EnumType.ORDINAL)
+    private Gender gender;
+
+    private Date birthDate;
+
+    private String province;
+
+    private String city;
+
+    private String district;
+
+    private String degree;
+
+    private boolean isTeacher;
+
     public User(){}
 
     public User(Long id, String username) {
         this.id = id;
         this.username = username;
+    }
+
+    public User(Long id, String username, String firstName, String lastName, Gender gender, Date birthDate, String province, String city, String district, String degree, boolean isTeacher) {
+        this.id = id;
+        this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.birthDate = birthDate;
+        this.province = province;
+        this.city = city;
+        this.district = district;
+        this.degree = degree;
+        this.isTeacher = isTeacher;
     }
 
     public Long getId() {
@@ -35,4 +69,75 @@ public class User {
         this.username = username;
     }
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public String getDegree() {
+        return degree;
+    }
+
+    public void setDegree(String degree) {
+        this.degree = degree;
+    }
+
+    public boolean isTeacher() {
+        return isTeacher;
+    }
+
+    public void setTeacher(boolean teacher) {
+        isTeacher = teacher;
+    }
 }
